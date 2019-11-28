84947a22981978ebd2eaacc16f6b1a0916820acd
Caoxinghao <864250343@qq.com>
2019-11-28 23:42:52 +0800

    添加zuul网关，主要是代理和路由，将所有的请求由zuul进行转发和过滤，zuul本身也是注册到eureka中

---
+++ microservicecloud-zuul-gateway-9527/src/main/java/com/xinghao/cloud/Zuul_9527_StartSpringCloudApp.java
@@ -0,0 +1,18 @@
+package com.xinghao.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author caoxinghao
+ * @Date 2019/11/28 23:21
+ * @Description:
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class Zuul_9527_StartSpringCloudApp {
+    public static void main(String[] args) {
+        SpringApplication.run(Zuul_9527_StartSpringCloudApp.class,args);
+    }
+}
